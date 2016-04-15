1779c7e84d4db601f1c7029041968aa631781eda
Laurent, David | ISDOD <ts-david.laurent@rakuten.com>
2016-04-15 14:25:24 +0900

    - add new command to filter lunch (best, vegi, ankit)
    type :
    - /bobo vegi lunch (to get vegetarian meals)
    - /bobo ankit lunch (to get meals with chicken)
    - /bobo best lunch (to get lunch with umai count)

--- src/bobo_bot/command_list.cr
+++ src/bobo_bot/command_list.cr
@@ -2,21 +2,27 @@ module BoboBot
   # Parses the command from hipchat
   class CommandList
 
-    def initialize
-      default = Commands::Default.new # First
-      lunch = Commands::Lunch.new
-      dinner = Commands::Dinner.new
-      slap = Commands::Slap.new
-      @hash = {
-        lunch.command =>    lunch,
-        dinner.command =>   dinner,
-        slap.command =>     slap,
-        default.command =>  default
-      }
-    end
+	def initialize
+	  default = Commands::Default.new # First
+	  lunch = Commands::Lunch.new
+	  bestlunch = Commands::BestLunch.new
+	  ankitlunch = Commands::AnkitLunch.new
+	  vegilunch = Commands::VegiLunch.new
+	  dinner = Commands::Dinner.new
+	  slap = Commands::Slap.new
+	  @hash = {
+		lunch.command =>		lunch,
+		bestlunch.command =>	bestlunch,
+		ankitlunch.command =>   ankitlunch,
+		vegilunch.command =>	vegilunch,
+		dinner.command =>	    dinner,
+		slap.command =>		    slap,
+		default.command =>	    default
+	  }
+	end
 
-    def list : Hash
-      @hash
-    end
+	def list : Hash
+	  @hash
+	end
   end
 end
---
+++ src/bobo_bot/commands/vegi_lunch.cr
@@ -0,0 +1,44 @@
+require "ecr/macros"
+module BoboBot
+  # Lunch  Response
+  module Commands
+	class VegiLunch < BaseCommand
+	  def initialize
+		@command = "vegi lunch"
+		time = (Time.utc_now + 9.hours).to_s("%Y%m%d") # Tokyo time
+		@lunch_response = ::LunchApi.get_data(time)
+		@Flr9 = @lunch_response.data.select do |d|
+			d.cafeteriaId == "9F" &&
+			!d.ingredients.beef &&
+			!d.ingredients.fish &&
+			!d.ingredients.mutton &&
+			!d.ingredients.pork &&
+			!d.ingredients.chicken &&
+		end
+		@Flr22 = @lunch_response.data.select do |d|
+			d.cafeteriaId == "22F" &&
+			!d.ingredients.beef &&
+			!d.ingredients.fish &&
+			!d.ingredients.mutton &&
+			!d.ingredients.pork &&
+			!d.ingredients.chicken &&
+		end
+	  end
+
+	  def json
+		{
+		  color: "blue",
+		  message: "<h2>vegetarian meals are:</h2> #{html}",
+		  notify: false,
+		  message_format: "html"
+		}.to_json
+	  end
+
+	  def html
+		io = MemoryIO.new
+		ECR.embed "src/bobo_bot/commands/lunch.ecr", io
+		io.to_s
+	  end
+	end
+  end
+end
